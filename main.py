79005fcebcd311d414e738327af6faeca4fa9351
developer1 <kokosh3180@naver.com>
2026-04-25 12:42:09 +0900

    Add btn2(Clear) and handler function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton,QVBoxLayout, QMessageBox, QPlainTextEdit)
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton,QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout)
 from PyQt5.QtGui import QIcon
 
 class Calculator(QWidget):
@@ -17,9 +17,18 @@ class Calculator(QWidget):
         self.btn1 = QPushButton('Message', self)
         self.btn1.clicked.connect(self.activateMessage)
 
+        self.btn2 = QPushButton('Clear',self)
+        self.btn2.clicked.connect(self.clearMessage)
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
+
         vbox = QVBoxLayout()
         vbox.addWidget(self.te1)
-        vbox.addWidget(self.btn1)
+        #vbox.addWidget(self.btn1)
+        vbox.addLayout(hbox)
         vbox.addStretch(1)
     
         self.setLayout(vbox)
@@ -28,6 +37,9 @@ class Calculator(QWidget):
         self.setWindowIcon(QIcon('icon.png'))   
         self.resize(256,256)
         self.show()
+
+    def clearMessage(self):
+        self.te1.clear()        
 
     def activateMessage(self):
         # QMessageBox.information(self,"infor","Button clicked!!!")  
